c6b945fb3d8cd19b01e33d424c1b4e36a667862a
Vinicius Santos <vinobeacoup56@gmail.com>
2026-01-26 00:38:06 -0300

    chore: setup database connection and engine

---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, Column, String, Numeric, DateTime, Integer
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime, timezone
+
+Base = declarative_base()
+URL_BANK = "postgresql://postgres:211508@localhost:5432/postgres"
+
+class Tabel(Base):
+    __tablename__ = 'History'
+    Id = Column(Integer, primary_key=True, autoincrement=True)
+    Product = Column(String, nullable=False)
+    Price = Column(String, nullable=False)
+    Date = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
+engine = create_engine(URL_BANK)
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
+
+def get_session():
+    return Session()
+
+
+
+
+
+
