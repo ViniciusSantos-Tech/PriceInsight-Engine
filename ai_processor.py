b21a738287af19a61db7ae070226cb7a932ef336
Vinicius Santos <vinobeacoup56@gmail.com>
2026-01-27 01:40:40 -0300

    Refactor IA class to simplify initialization

--- ai_processor.py
+++ ai_processor.py
@@ -1,15 +1,10 @@
 import os
 import json
 from groq import Groq
-import ollama
 
 class IA():
     def __init__(self):
-        self.api_key = os.getenv("GROQ_API_KEY")
-        if self.api_key:
-            self.client = Groq(api_key=self.api_key)
-        else:
-            self.ollama = ollama
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
     def analyze_with_ai(self, raw_data):
         prompt = f"""
@@ -22,17 +17,11 @@ class IA():
         Data: {raw_data}
         """
         
-        if self.api_key:
-            completion = self.client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
-                messages=[{"role": "user", "content": prompt}]
-            )
-            return completion.choices[0].message.content
-        else:
-            response = self.ollama.chat(model='gemma3:4b', messages=[
-                {'role': 'user', 'content': prompt},
-            ])
-            return response['message']['content']
+        completion = self.client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[{"role": "user", "content": prompt}]
+        )
+        return completion.choices[0].message.content
 
     def format_response(self, response):
         try:
