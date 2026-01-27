eaebd6bf3633e7cb3719e71992c9969df2813b1c
Vinicius Santos <vinobeacoup56@gmail.com>
2026-01-26 23:39:54 -0300

    Refactor imports and update IA method names

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from scrap import Scrap
-from processador_ia import IA
+from ai_processor import IA
 from crud import Crud
 
 app_scrapping = Scrap()
 scrapping_response = app_scrapping.Scraping()
 
 app_ia = IA()
-ia_response = app_ia.processar_com_ia(scrapping_response)
-dics_dates = app_ia.limpar_resposta_ia(ia_response)
+ia_response = app_ia.analyze_with_ai(scrapping_response)
+dics_dates = app_ia.format(ia_response)
 
 
 app_crud = Crud()
