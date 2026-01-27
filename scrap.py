3232882b38a7cfde8e6186f86c51cc0bfab83dab
Vinicius Santos <vinobeacoup56@gmail.com>
2026-01-27 00:16:09 -0300

    Enhance Chrome options for web scraping
    Added headless mode and additional options for Chrome.

--- scrap.py
+++ scrap.py
@@ -11,6 +11,9 @@ class Scrap():
         self.options = webdriver.ChromeOptions()
         self.options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         self.options.add_argument("--disable-blink-features=AutomationControlled")
+        self.options.add_argument("--headless=new")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-dev-shm-usage")
         self.url = "https://www.mercadolivre.com.br/smartphone-motorola-moto-g06-128gb-12gb-4gb-ram-8gb-ram-boost-e-camera-50mp-com-ai-bateria-de-5200-mah-tela-69-azul-marinho/p/MLB58353028#polycard_client=search-desktop&search_layout=grid&position=1&type=product&tracking_id=7889f463-4084-46f1-8a63-1b8a7fb0429b&wid=MLB4228627923&sid=search"
     def Scraping(self):
         self.driver = webdriver.Chrome(options=self.options)
@@ -26,6 +29,7 @@ class Scrap():
         finally:
             self.driver.quit()
         
+
 
 
 
