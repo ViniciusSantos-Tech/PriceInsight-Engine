208aaf39137befd3840b49126062619795066cc7
Vinicius Santos <vinobeacoup56@gmail.com>
2026-02-04 23:17:52 -0300

    Update ai_processor.py

--- ai_processor.py
+++ ai_processor.py
@@ -2,14 +2,13 @@ import os
 import json
 from dotenv import load_dotenv
 from groq import Groq
-
 load_dotenv()
 
 class IA():
     def __init__(self):
         self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-    def analyze_with_ai(self, raw_data):
+    def analyze_with_ai(self, raw_data: str) -> tuple[bool, str, str]:
         try:
             prompt = f"""
             Your task is to extract the CURRENT CASH PRICE (PIX/Boleto) from the provided HTML data and convert it into a structured JSON.
@@ -17,9 +16,9 @@ class IA():
             STRICT RULES:
             1. Return ONLY the raw JSON. No markdown blocks (```json), no explanations, no conversational text.
             2. Extract ONLY the single lowest cash price available. Ignore installment prices (parcelas) or total credit prices.
-            3. Format: {{"Status": "Success", "Price": "xxxxx.xx"}}
-            4. Numeric Format: Use ONLY digits and a single period (.) as a decimal separator. Remove currency symbols (R$), thousands separators (.), and commas (,).
-            - Correct: "xxxxx.xx"
+            3. Format: {{"Status": "Success", "Price": "xx.xxx"}}
+            4. Numeric Format: Use ONLY digits and a single period (.) as a decimal separator. Remove currency symbols (R$) and commas (,).
+            - Correct: "xx.xxx"
             - Incorrect: "xx.xxx,xx" or "R$ xx.xxx,xx"
             5. If the price cannot be clearly found, return: {{"Status": "Failure", "Price": "None"}}
 
@@ -31,14 +30,14 @@ class IA():
                 model="llama-3.3-70b-versatile",
                 messages=[{"role": "user", "content": prompt}]
             )
-            return completion.choices[0].message.content
+            return True, "Analyzed successfully.", completion.choices[0].message.content
         except Exception as e:
-            return f"Error: {e}"
+            return False, f"Error: {e}", ""
     
 
-    def format_response(self, response):
+    def format_response(self, response: str) -> dict:
         try:
             cleaned = response.replace("```json", "").replace("```", "").strip()
-            return json.loads(cleaned)
+            return True, "sucess", json.loads(cleaned)
         except:
-            return "Error!"
+            return False, "Error!", {"Price": "0"}
